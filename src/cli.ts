914c24c8e866d6d3c0a44bd09a65c191038ec2f1
BWsix <itisbw62@gmail.com>
2021-11-21 22:44:38 +0800

    fix(cli): rolled back to previous version

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 
 import yargs from "yargs";
-import { bootstrap } from "./main";
 import { getAuthUrl } from "./spotifyApiProvider/cli/getAuthUrl";
 import { getToken } from "./spotifyApiProvider/cli/getToken";
 
 yargs
   .scriptName("spotify-now")
-  .command("start", "start the app", {}, bootstrap)
   .command(
     "login [id] [secret]",
     "log in to your spotify app.",
--- src/main.ts
+++ src/main.ts
@@ -22,3 +22,5 @@ export async function bootstrap() {
     board.loop(30 * 1000, handleColor);
   });
 }
+
+bootstrap();
