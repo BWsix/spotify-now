fa65fdf3ad996d0e184e832222dae186715a307d
BWsix <itisbw62@gmail.com>
2021-11-21 21:21:06 +0800

    fix(main): IDK what's going on here, but this helps

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,19 @@ import { getColor } from "./core";
 import { refreshTokenWhenExpire } from "./spotifyApiProvider";
 
 const board = new Board({ repl: false });
-const anode = new Led.RGB({
-  pins: [11, 10, 9],
-});
-
-const handleColor = async () => {
-  const color = await refreshTokenWhenExpire(getColor);
-
-  // the type definition on this method is wrong. (should accept type of number[])
-  anode.color((color as unknown as string) || "000000");
-};
 
 board.on("ready", async () => {
+  const anode = new Led.RGB({
+    pins: [11, 10, 9],
+  });
+
+  const handleColor = async () => {
+    const color = await refreshTokenWhenExpire(getColor);
+
+    // the type definition on this method is wrong. (should accept type of number[])
+    anode.color((color as unknown as string) || "000000");
+  };
+
   await handleColor();
   anode.on();
   board.loop(30 * 1000, handleColor);
